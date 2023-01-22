d86f833bf26a408427fd796feb2714119b1300ea
Niloy Farhan <79375262+Variable-N@users.noreply.github.com>
2023-01-23 00:21:14 +0600

    First Commit

---
+++ Main.py
@@ -0,0 +1,106 @@
+import cv2 as cv
+import numpy as np
+import os
+from time import time
+import win32gui
+import win32ui
+import win32con
+
+def window_capture():
+    w = 1920 
+    h = 1080 
+
+    # hwnd = win32gui.FindWindow(None, windowname) # For any specific window
+    hwnd = None
+
+    # Get the window image data
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj=win32ui.CreateDCFromHandle(wDC)
+    cDC=dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt( (0, 0), (w, h) , dcObj, (0, 0), win32con.SRCCOPY)
+
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (h,w,4)
+
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+
+    img = img[..., :3]
+
+    return  img
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+fps = time()
+
+
+net = cv.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+def yolov3(img):
+    img = cv.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    blob = cv.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    font = cv.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[i]
+            cv.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv.putText(img, label, (x, y + 30), font, 3, color, 3)
+    
+    return img
+
+while True:
+    cv.imshow('Computer vision',yolov3(window_capture()))
+    print('FPS: ', 1/(time()-fps))
+    fps = time()
+
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print('Done')
